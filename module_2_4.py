d239e7d720ae231fb9a19c93f9fe28e66121ff8d
NikolayMesh <partizan.91@bk.ru>
2024-08-23 14:21:01 +0300

    add 100

--- module_2_4.py
+++ module_2_4.py
@@ -1,7 +1,11 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]
 primes = []
 not_primes = []
-devid =[2, 3, 5]
+devid =[2, 3, 5, 7]
+coun = 30
+while coun <= 100:
+    coun = coun + 1
+    numbers.append(coun)
 for i in range(1, len(numbers)):
     for j in devid:
         is_prim = False
@@ -16,3 +20,5 @@ for i in range(1, len(numbers)):
         primes.append(numbers[i])
 print('primes ', primes)
 print('not_primes ', not_primes)
+
+
