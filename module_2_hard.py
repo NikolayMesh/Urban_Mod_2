c5ba24ffc88ed1ad114a163130f591ca84051a41
NikolayMesh <partizan.91@bk.ru>
2024-09-03 16:00:01 +0300

    add-

--- module_2_hard.py
+++ module_2_hard.py
@@ -8,20 +8,3 @@ if 3 <= number <= 20:
     print(f"Пароль для числа {number}: {result}")
 else:
     print("Число должно быть от 3 до 20")
-
-
-
-def generate_password(n):
-    result = ""
-    for i in range(1, n // 2 + 1):
-        for j in range(i + 1, n):
-            if (i + j) % n == 0:
-                result += str(i) + str(j)
-    return result
-
-n = int(input("Введите число от 3 до 20: "))
-if 3 <= n <= 20:
-    password = generate_password(n)
-    print(f"Пароль для числа {n}: {password}")
-else:
-    print("Число должно быть от 3 до 20")
